7e7e1d0074c0134934fb41649d5a5685c22902bd
David Gomes <davidrafagomes@gmail.com>
2024-09-27 18:52:15 +0200

    more simplification

--- app/actions.tsx
+++ app/actions.tsx
@@ -6,25 +6,23 @@ import { Todo } from "@/app/schema";
 import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
-function parseJwt(token: string) {
-  return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
-}
-
 export async function insertTodo({ newTodo }: { newTodo: string }) {
-  const { getToken } = auth();
+  const { getToken, userId } = auth();
   const authToken = await getToken();
 
   if (!authToken) {
     throw new Error("No auth token");
   }
 
-  const todo = await getDb(authToken)
-    .insert(schema.todos)
-    .values({
-      task: newTodo,
-      userId: parseJwt(authToken)["sub"],
-      isComplete: false,
-    });
+  if (!userId) {
+    throw new Error("No user id");
+  }
+
+  await getDb(authToken).insert(schema.todos).values({
+    task: newTodo,
+    userId,
+    isComplete: false,
+  });
 
   revalidatePath("/");
 }
